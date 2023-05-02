c2931099b8a7e14d1046607c71e50d5d1fbd8caa
viktorK1990 <korotchinV1990@yandex.ru>
2023-05-02 14:34:36 +0800

    Реализовать собственную структуру данных - HashMap

--- src/main/java/ru/job4j/collection/map/SimpleMap.java
+++ src/main/java/ru/job4j/collection/map/SimpleMap.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection.map;
 
-import java.util.Iterator;
+import java.util.*;
 
 public class SimpleMap<K, V> implements Map<K, V> {
 
@@ -16,35 +16,102 @@ public class SimpleMap<K, V> implements Map<K, V> {
 
     @Override
     public boolean put(K key, V value) {
-        return false;
+        var result = false;
+        if (count == capacity) {
+            expand();
+        }
+        if (key != null) {
+            var index = indexFor(hash(key.hashCode()));
+            table[index] = new MapEntry<>(key, value);
+            result = true;
+        } else {
+            table[0] = new MapEntry<>(null, value);
+            result = true;
+        }
+        count++;
+        modCount++;
+
+        return result;
     }
 
     private int hash(int hashCode) {
-        return 0;
+        return hashCode ^ (hashCode >>> 16);
     }
 
     private int indexFor(int hash) {
-        return 0;
+        return hash % capacity;
     }
 
     private void expand() {
+        MapEntry<K, V>[] oldTable = table;
+        capacity *= 2;
+        for (MapEntry<K, V> e : oldTable) {
+            put(e.key, e.value);
+        }
 
     }
 
     @Override
     public V get(K key) {
-        return null;
+        V element = null;
+        var index = indexFor(hash(key.hashCode()));
+        if (table[index] != null) {
+            if (table[index].key.hashCode() == key.hashCode() && table[index].key.equals(key)) {
+                element = table[index].value;
+            }
+        }
+        return element;
     }
 
     @Override
     public boolean remove(K key) {
-        return false;
+        boolean remove = false;
+        var index = indexFor(hash(key.hashCode()));
+        if (table[index] != null) {
+            if (table[index].key.hashCode() == key.hashCode() && table[index].key.equals(key)) {
+                table[index] = null;
+                remove = true;
+                count--;
+                modCount++;
+            }
+        }
+        return remove;
     }
 
     @Override
     public Iterator<K> iterator() {
-        return null;
+        return new Iterator<K>() {
+            int point = 0;
+            int expectedCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                var result = false;
+                if (expectedCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                if (point < table.length) {
+                    for (int i = point; i < table.length; i++) {
+                        if (table[i] != null) {
+                            point = i;
+                            result = true;
+                            break;
+                        }
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return table[point++].key;
+            }
+        };
     }
+
 
     private static class MapEntry<K, V> {
 
@@ -55,5 +122,22 @@ public class SimpleMap<K, V> implements Map<K, V> {
             this.key = key;
             this.value = value;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            MapEntry<?, ?> mapEntry = (MapEntry<?, ?>) o;
+            return Objects.equals(key, mapEntry.key);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(key);
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
---
+++ src/test/java/ru/job4j/collection/map/SimpleMapTest.java
@@ -0,0 +1,81 @@
+package ru.job4j.collection.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.*;
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleMapTest {
+
+    private final SimpleMap<Integer, String> map = new SimpleMap<>();
+
+    @BeforeEach
+    void setUp() {
+        map.put(1, "1");
+        map.put(2, "2");
+        map.put(3, "3");
+        map.put(4, "4");
+     }
+
+    @Test
+    void checkSimpleIterator() {
+        assertThat(map).hasSize(4).contains(1, 2, 3, 4);
+    }
+
+    @Test
+    void whenCheckGet() {
+        assertThat(map.get(1)).isEqualTo("1");
+        assertThat(map).hasSize(4);
+        assertThat(map.get(5)).isNull();
+        assertThat(map).hasSize(4);
+    }
+
+
+    @Test
+    void whenCheckRemove() {
+        assertThat(map.remove(2)).isTrue();
+        assertThat(map).hasSize(3);
+        assertThat(map.remove(2)).isFalse();
+        assertThat(map).hasSize(3);
+        assertThat(map.remove(5)).isFalse();
+        assertThat(map).hasSize(3);
+    }
+
+    @Test
+    void whenCheckIterator() {
+        map.remove(2);
+        map.remove(3);
+        map.put(null, "0000");
+        Iterator<Integer> it = map.iterator();
+        assertThat(it.hasNext()).isTrue();
+        assertThat(it.next()).isNull();
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(4);
+        assertThat(it.hasNext()).isFalse();
+        assertThatThrownBy(it::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void whenConcurrentIteratorAdd() {
+        Iterator<Integer> it = map.iterator();
+        map.put(0, "0");
+        assertThatThrownBy(it::hasNext)
+                .isInstanceOf(ConcurrentModificationException.class);
+    }
+
+    @Test
+    void whenConcurrentIteratorRemove() {
+        Iterator<Integer> it = map.iterator();
+        map.remove(1);
+        assertThatThrownBy(it::hasNext)
+                .isInstanceOf(ConcurrentModificationException.class);
+    }
+
+    @Test
+    void whenNotConcurrentIteratorGet() {
+        Iterator<Integer> it = map.iterator();
+        map.get(1);
+        assertThat(it.hasNext()).isTrue();
+    }
+}
